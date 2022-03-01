edd01624fd10327f753c21b30b41976941024797
Ravi Mendis <mendismeister@gmail.com>
2022-03-01 12:05:28 +0000

    Fix tundra

--- Maps/NewWorld_XP2.lua
+++ Maps/NewWorld_XP2.lua
@@ -568,7 +568,7 @@ function AddFeatures()
 	-- Get Rainfall setting input by user.
 	local rainfall = MapConfiguration.GetValue("rainfall");
 	
-	local args = {rainfall = rainfall, iJunglePercent = 40, iMarshPercent = 7, iForestPercent = 40, iIcePercent = 12}
+	local args = {rainfall = rainfall, iJunglePercent = 40, iMarshPercent = 7, iForestPercent = 40}
 	featuregen = FeatureGenerator.Create(args);
 
 	featuregen:AddFeatures();
@@ -640,6 +640,9 @@ function GenerateTerrainTypesEarth(plotTypes, iW, iH, iFlags, bNoCoastalMountain
 
 			-- arctic circle and patagonia
 			elseif (_lat > 0.73 or _lat < -0.66) then
+				iTundraTop = earth:GetHeight(45);
+				iTundraBottom = earth:GetHeight(0);
+
 				if (plotTypes[index] == g_PLOT_TYPE_MOUNTAIN) then
 					terrainTypes[index] = g_TERRAIN_TYPE_SNOW_MOUNTAIN;
 
@@ -686,6 +689,12 @@ function GenerateTerrainTypesEarth(plotTypes, iW, iH, iFlags, bNoCoastalMountain
 
 			-- grassland not found further north than 60 deg.
 			elseif (lat > 0.66) then
+				iTundraTop = earth:GetHeight(45);										
+				iTundraBottom = earth:GetHeight(5);
+
+				iPlainsTop = iTundraBottom;
+				iPlainsBottom = earth:GetHeight(0);
+
 				if (plotTypes[index] == g_PLOT_TYPE_MOUNTAIN) then
 					terrainTypes[index] = g_TERRAIN_TYPE_SNOW_MOUNTAIN;
 
